ff6749a1c38976c1ada4e8673ee8a01657da4aa7
Minh Duy <leduy06072003@gmail.com>
2023-09-19 23:48:00 +0700

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            A2.2110M
+            Minh Duy
         </p>
         <a
           className="App-link"
@@ -23,3 +23,6 @@ function App() {
 }
 
 export default App;
+
+
+
