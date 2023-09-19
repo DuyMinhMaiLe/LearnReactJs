5203f50016690fb0d90bb5e05dfc1d0319415d0d
Minh Duy <leduy06072003@gmail.com>
2023-09-19 19:48:45 +0700

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Le Mai Minh Duy
+            A2.2110M
         </p>
         <a
           className="App-link"
